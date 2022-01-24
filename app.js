f1e9c82e9b4446fac0ad5383ae294af06de7bd9c
Sebastian Künne <sebuzz@mac.com>
2022-01-24 12:38:23 +0100

    feature: prettify json

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
+// import { promises as fs } from "fs";
+
+import { writeFile } from 'fs/promises';
+
+
 console.log("Hello World");
+
+writeFile("foo", "Axel der Kater");
+
+writeFile("hello.json", JSON.stringify({"x": 5, "y": 6}, null, 4));
