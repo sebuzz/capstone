2f44eaa1941de74d04199759deae6c80e15ae90b
Sebus <sebuzz@mac.com>
2022-01-25 01:18:44 +0100

    feature: added args.js that prints out just the args to console (#16)
    closes #15

--- args.js
+++ args.js
@@ -1,3 +1,12 @@
-args.js
+
 // expected
 // node args.js foo bar baz
+
+
+import { argv } from 'process';
+
+const myArgs = function() {
+    argv.splice(0,2);
+    return argv;
+}
+console.log(myArgs().join(" "));
